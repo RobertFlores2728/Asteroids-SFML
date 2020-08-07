78cf52835e83642d51230a6c5637411913a794ae
RobertFlores2728 <68761226+RobertFlores2728@users.noreply.github.com>
2020-08-07 15:29:03 -0500

    Added ship velocity and rotation
    -ship travels in direction it looks in using its angle and cos, sine functions which can be used to make a forward vector which is then added to the velocity

--- Asteroids_SFML/Asteroids_SFML/Ship.cpp
+++ Asteroids_SFML/Asteroids_SFML/Ship.cpp
@@ -1,17 +1,25 @@
 #include "Ship.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
 
 
 Ship::Ship() {
+    
     triangle.setPointCount(3);
-    triangle.setPoint(0, sf::Vector2f(triangleSize / 2, 0));
-    triangle.setPoint(1, sf::Vector2f(triangleSize, triangleSize));
+    triangle.setPoint(0, sf::Vector2f(triangleSize, triangleSize/2));
+    triangle.setPoint(1, sf::Vector2f(0, 0));
     triangle.setPoint(2, sf::Vector2f(0, triangleSize));
+    triangle.setOrigin(triangleSize/2, triangleSize/2);
     triangle.setOutlineColor(sf::Color::White);
     triangle.setOutlineThickness(3);
-    triangle.setPosition(500, 500);
+    position.x = 500;
+    position.y = 500;
     triangle.setFillColor(sf::Color::Black);
+
+    triangle.setRotation(0.f); // rotations in sfml go in clockwise fashion
+    PrintRotation();
+    
 }
 
 void Ship::Draw(sf::RenderWindow& window) {
@@ -34,7 +42,11 @@ void Ship::Move(sf::Time deltaTime) {
             velocity = 0;
     }
 
-    position.y = position.y + (velocity * deltaTime.asSeconds());
+    sf::Vector2f forwardVector;
+    forwardVector.x = std::cos(triangle.getRotation() * 3.141592 / 180.0); //cmath cos and sin functions take in radians but getRotation returns degrees. radians = degrees * pi / 180
+    forwardVector.y = -std::sin(triangle.getRotation() * 3.141592 / 180.0); // negative since sfml rotations are in clockwise
+    position.x = position.x + (velocity * forwardVector.x * deltaTime.asSeconds());
+    position.y = position.y - (velocity * forwardVector.y * deltaTime.asSeconds());
     triangle.setPosition(position.x, position.y);
 }
 
@@ -45,4 +57,20 @@ void Ship::GetInput(sf::Time deltaTime) {
     else {
         isThrusting = false;
     }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        triangle.setRotation(triangle.getRotation() - (rotationSpeed * deltaTime.asSeconds()));
+        PrintRotation();
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        triangle.setRotation(triangle.getRotation() + (rotationSpeed * deltaTime.asSeconds()));
+        PrintRotation();
+    }
+}
+
+void Ship::PrintRotation() {
+    std::cout << "Angle:" << triangle.getRotation();
+    std::cout << " Cosine: " << std::cos(triangle.getRotation() * 3.141592 / 180.0);
+    std::cout << " Sine: " << std::sin(triangle.getRotation() * 3.141592 / 180.0) << std::endl;
 }
--- Asteroids_SFML/Asteroids_SFML/Ship.h
+++ Asteroids_SFML/Asteroids_SFML/Ship.h
@@ -5,15 +5,17 @@
 class Ship
 {
 	//triangle
-	int triangleSize = 100;
+	int triangleSize = 30;
 	sf::ConvexShape triangle;
 
 	sf::Vector2<float> position;
 
+	float rotationSpeed = 150.f;
+
 	float velocity;
 
 	bool isThrusting = false;
-	float thrust = 100;
+	float thrust = 250;
 
 	float drag = 30;
 
@@ -23,5 +25,7 @@ public:
 	void Update(sf::Time deltaTime);
 	void Move(sf::Time deltaTime);
 	void GetInput(sf::Time deltaTime);
+
+	void PrintRotation();
 };
 
